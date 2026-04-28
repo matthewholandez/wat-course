5b5d7a4bb1db499868cae67746dc0039886a1676
Matthew Holandez <91989998+matthewholandez@users.noreply.github.com>
2026-04-28 16:44:34 -0400

    feat: create about page & metadata

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next"
+
+export default function AboutPage() {
+    return (
+        <>
+            <div>Yo</div>
+        </>
+    )
+}
+
+export const metadata: Metadata = {
+    title: 'About'
+}
