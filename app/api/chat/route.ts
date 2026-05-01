3b7583f2edbf646ef19638f101700f602b84820e
Matthew Holandez <91989998+matthewholandez@users.noreply.github.com>
2026-05-01 14:16:57 -0400

    feat: working chatbot

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -2,6 +2,7 @@ import { OpenRouter } from "@openrouter/sdk";
 import RedisSingleton from "@/lib/redis";
 
 import type { MessageRequest } from "@/hooks/useChatEngine";
+import { ChatMessages } from "@openrouter/sdk/models";
 
 export const runtime = "nodejs";
 
@@ -23,19 +24,27 @@ export async function POST(req: Request) {
 
         const redis = await RedisSingleton.getInstance();
 
+        const exists = await redis.exists(`messages:${conversationId}`);
+        let messagesToSend: ChatMessages[] = [
+            {
+                content: "You are a Student Advisor at the University of Waterloo. Also, you're a goose. Respond to the student's question, concisely, and do not use any markdown. You may use humour conservatively.",
+                role: "system"
+            }
+        ]
+        if (exists) {
+            const history = await redis.lRange(`messages:${conversationId}`, 0, -1);
+            const parsedHistory = history.map(m => JSON.parse(m));
+            messagesToSend.push(...parsedHistory);
+        }
+        messagesToSend.push({
+            content: content,
+            role: 'user'
+        })
+
         const stream = await openRouterClient.chat.send({
             chatRequest: {
                 maxTokens: 1000,
-                messages: [
-                    {
-                        content: "You are a Student Advisor at the University of Waterloo. Also, you're a goose. Respond to the student's question, concisely, and do not use any markdown. You may use humour conservatively.",
-                        role: "system"
-                    },
-                    {
-                        content: content,
-                        role: "user"
-                    }
-                ],
+                messages: messagesToSend,
                 model: process.env.MODEL,
                 temperature: 0.7,
                 stream: true
@@ -44,10 +53,12 @@ export async function POST(req: Request) {
 
         const readableStream = new ReadableStream({
             async start(controller) {
+                let finalAssistantResponse = "";
                 const encoder = new TextEncoder();
                 try {
                     for await (const chunk of stream) {
                         const content = chunk.choices?.[0]?.delta?.content;
+                        finalAssistantResponse += content;
                         if (content) {
                             controller.enqueue(encoder.encode(content));
                         }
@@ -55,6 +66,14 @@ export async function POST(req: Request) {
                 } catch (error) {
                     controller.error(error);
                 } finally {
+                    await redis.rPush(`messages:${conversationId}`, JSON.stringify({
+                        content: content,
+                        role: 'user',
+                    }))
+                    await redis.rPush(`messages:${conversationId}`, JSON.stringify({
+                        content: finalAssistantResponse,
+                        role: 'assistant',
+                    }))
                     controller.close();
                 }
             },
